ff4e879a5380839076e89e954163c4c94b8e520f
ImaMultiDev <imamultidev@gmail.com>
2025-04-15 13:38:01 +0200

    refactor(app\lib\actions.ts): Validate and prepare form data to avoid errors, import zod to facilitate validation, amountInCents to avoid errors with float values in JS

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,24 @@
 "use server";
 
+import { z } from "zod";
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
-  };
-  console.log(rawFormData);
+  });
+  // Almacenar los valores monetarios en centavos en la base de datos para
+  // eliminar errores de punto flotante de JavaScript y garantizar una mayor precisión
+  const amountInCents = amount * 100;
 }
